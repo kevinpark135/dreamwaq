5bb070f5329577d5cc0194ff4fc86e1f47c69031
kevinpark135 <kevinpark135@naver.com>
2026-06-12 15:10:25 +0900

    Set up DreamWaQ A1 baseline config

--- dreamwaq_env_cfg.py
+++ dreamwaq_env_cfg.py
@@ -1,7 +1,38 @@
 from isaaclab.utils.configclass import configclass
+from isaaclab.managers import ObservationGroupCfg as ObsGroup
+from isaaclab.managers import ObservationTermCfg as ObsTerm
+from isaaclab.managers import SceneEntityCfg
+
+import isaaclab_tasks.manager_based.locomotion.velocity.mdp as mdp
 
 from .rough_env_cfg import UnitreeA1RoughEnvCfg, UnitreeA1RoughEnvCfg_PLAY
 
+@configclass
+class DreamWaQCriticObsCfg(ObsGroup):
+    """Privileged observations for DreamWaQ critic."""
+
+    # basic proprioception
+    base_lin_vel = ObsTerm(func=mdp.base_lin_vel)
+    base_ang_vel = ObsTerm(func=mdp.base_ang_vel)
+    projected_gravity = ObsTerm(func=mdp.projected_gravity)
+    velocity_commands = ObsTerm(
+        func=mdp.generated_commands,
+        params={"command_name": "base_velocity"},
+    )
+    joint_pos = ObsTerm(func=mdp.joint_pos_rel)
+    joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+    actions = ObsTerm(func=mdp.last_action)
+
+    # privileged info
+    height_scan = ObsTerm(
+        func=mdp.height_scan,
+        params={"sensor_cfg": SceneEntityCfg("height_scanner")},
+        clip=(-1.0, 1.0),
+    )
+
+    def __post_init__(self):
+        self.enable_corruption = False
+        self.concatenate_terms = True
 
 @configclass
 class DreamWaQA1RoughEnvCfg(UnitreeA1RoughEnvCfg):
@@ -18,8 +49,11 @@ class DreamWaQA1RoughEnvCfg(UnitreeA1RoughEnvCfg):
         self.rewards.dof_torques_l2.weight = -2.0e-5
         self.rewards.action_rate_l2.weight = -0.01
 
-        # remove exteroceptive observations
+        # actor: remove exteroceptive observations
         self.observations.policy.height_scan = None
+
+        # critic: privileged observation
+        self.observations.critic = DreamWaQCriticObsCfg()
 
 
 @configclass
